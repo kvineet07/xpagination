7635562ae68f2f4df0c63e3dfa4c37e65dc04865
kvineet07 <kumarvineet3445@gmail.com>
2024-09-28 17:14:52 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [employees, setEmployees] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setEmployees(data);
+      } catch (error) {
+        alert("Failed to fetch data");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Calculate the index of the first item of the current page
+  const indexOfLastEmployee = currentPage * itemsPerPage;
+  const indexOfFirstEmployee = indexOfLastEmployee - itemsPerPage;
+  const currentEmployees = employees.slice(
+    indexOfFirstEmployee,
+    indexOfLastEmployee
+  );
+
+  // Handle page change
+  const nextPage = () => {
+    if (currentPage < Math.ceil(employees.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const previousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Employee Data Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentEmployees.map((employee, index) => (
+            <tr key={index}>
+              <td>{employee.id}</td>
+              <td>{employee.name}</td>
+              <td>{employee.email}</td>
+              <td>{employee.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="buttoncontainer">
+        <button onClick={previousPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <button> {currentPage} </button>
+        <button
+          onClick={nextPage}
+          disabled={currentPage === Math.ceil(employees.length / itemsPerPage)}
         >
-          Learn React
-        </a>
-      </header>
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
